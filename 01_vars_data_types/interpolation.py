8cc72a0bd6b047b31727f1c9f615355abe26ce98
ravi2krishna <ravi2krishna@gmail.com>
2025-07-22 09:13:52 +0530

    Concatenation & Interpolation

---
+++ 01_vars_data_types/interpolation.py
@@ -0,0 +1,9 @@
+name = "Ravi"
+age = 30
+new_age = 5
+
+# print("My Name is " +name + " im " + age+ " years old") # Concatenation
+# TypeError: can only concatenate str (not "int") to str
+print(f"My Name is {name} and im {age+new_age} years old") # Interpolation
+
+
