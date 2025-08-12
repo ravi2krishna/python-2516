728620999dbb7989e486669b0b4cb79c05685fe0
ravi2krishna <ravi2krishna@gmail.com>
2025-08-12 10:21:51 +0530

    Add Student

---
+++ 09_lms_ds/app.py
@@ -0,0 +1,80 @@
+# LMS -> Sub Feature - Student Management System
+# System Information --> Tuples
+SYSTEM_INFO = ("LMS Students Portal","v1.0","2025","Edify University")
+ADMIN_INFO = ("admmin@edify.ai","9876543210","201")
+
+# display system info
+print("="*50)
+print(f"Welcome To {SYSTEM_INFO[0]}")
+print(f"Developed By {SYSTEM_INFO[3]}")
+print("="*50)
+
+# store all students --> Dictionary
+students = {}
+
+# Start with option selection
+while True:
+    print("Choose an option from (1-5): ")
+    print("1 - Add Student")
+    print("2 - Update Student")
+    print("3 - Delete Student")
+    print("4 - List All Students")
+    print("5 - Exit System")
+    
+    choice = input("Enter Your Choice: ")
+    
+    if choice == "1":
+        print("Performing Operation 1")
+        student_id = input("Enter Student ID: ")
+        # student exists
+        if student_id in students:
+            print("Student Already with this ID exists in system!")
+        else:
+            name = input("Enter Student Name: ").title()
+            # store multiple scores
+            scores = []
+            while True:
+                score_input = input("Enter a score or type done: ")
+                # validate if input is number or done
+                if score_input == "done":
+                    break
+                if score_input.isdigit():
+                    score = int(score_input)
+                    if 0 <= score <= 100:
+                        scores.append(score)
+                    else:
+                        print("Score should be between 0-100")
+                else:
+                   print("Score should be Number Only") 
+        
+            # store multiple unique skills
+            skills = set()
+            while True:
+                skill_input = input("Enter a skill or type done: ")
+                if skill_input == "done":
+                    break
+                skills.add(skill_input.strip().title())
+            
+            # save student details received so far
+            students[student_id] = {
+                "name": name,
+                "scores": scores,
+                "skills": skills
+            }
+            print("Student Added Successfully!")
+            
+            # For Verification print student
+            print(students)
+            
+                       
+    elif choice == "2":
+        print("Performing Operation 2")
+    elif choice == "3":
+        print("Performing Operation 3")
+    elif choice == "4":
+        print("Performing Operation 4")
+    elif choice == "5":
+        print("Performing Operation 5")
+        break
+    else:
+       print("Invalid Choice Only (1-5) available") 
